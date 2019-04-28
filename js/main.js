d24d8feca2ae81730566f51badb3b148f8a8d1d0
Lhourquin <lhourquin@gmail.com>
2019-04-28 19:23:32 +0200

    create instance

---
+++ js/main.js
@@ -0,0 +1,21 @@
+new Vue ({
+    el: "#game",
+    data : {
+        pointYou : 100,
+        pointMonster : 100
+    },
+    methods : {
+        attack : function () {
+
+        },
+        special : function () {
+
+        },
+        heal : function () {
+
+        },
+        give : function () {
+            
+        }
+    }
+})
